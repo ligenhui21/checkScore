e88b0c55fcb1750cfde46a1fffe8625415f83e95
ligenhui <ligenhui21@163.com>
2017-11-13 16:52:19 +0800

    检查分数

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -1,16 +1,23 @@
 package service;
 
+import dao.IClazz;
 import dao.IStudent;
+import entity.Clazz;
 import entity.Student;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     private static SqlSessionFactory sqlSessionFactory;
     private static Reader reader;
 
@@ -18,7 +25,7 @@ public class Main {
         try {
             reader = Resources.getResourceAsReader("configure.xml");
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-            sqlSessionFactory.getConfiguration().addMapper(IStudent.class);
+            //sqlSessionFactory.getConfiguration().addMapper(IClazz.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,11 +37,17 @@ public class Main {
 
     public static void testInsert(){
         SqlSession session = sqlSessionFactory.openSession();
-        IStudent iStudent = session.getMapper(IStudent.class);
-        Student student = new Student();
+        List<Clazz> list = null;
+        try {
+            list = ReadExcel.readClzz();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        session.insert("clazzMapper.insertClazzBatch", list);
+       /* Student student = new Student();
         student.setStudentName("test");
-        iStudent.insertStudent(student);
-        session.commit();;
+        iStudent.insertStudent(student);*/
+        session.commit();
     }
 
     public static void main(String[] args) {
