1b9fdf3ea9df67c323b851de908b84625f49b5fa
ligenhui <ligenhui21@163.com>
2017-11-13 16:51:07 +0800

    检查分数首次提交

--- src/main/java/dao/IStudent.java
+++ src/main/java/dao/IStudent.java
@@ -5,5 +5,5 @@ import org.apache.ibatis.annotations.Insert;
 
 public interface IStudent {
     @Insert("INSERT INTO student(student_name) VALUES (#{studentName})")
-    public void insertStudent(Student student);
+    void insertStudent(Student student);
 }
